c4c3534f6d80be27a6d889617c566761c1e99c34
Palazov <Palazov@DESKTOP-J833GHP>
2021-11-07 22:45:10 +0100

    identifying the Web elements with id and name locators

--- Introduction/src/Selintroduction.java
+++ Introduction/src/Selintroduction.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Selintroduction {
 
@@ -9,14 +11,25 @@ public class Selintroduction {
 		//Invoking Browser
 		
 		//chromedriver.exe->Chrome Browser
-		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
-		
+		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");		
 		//webdriver.chrome.driver->value path(key)
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.freelancer.com/");
-		System.out.println(driver.getTitle());
-		System.out.println(driver.getCurrentUrl());
-		System.out.println(driver.getPageSource());
+		
+		
+		//FireFoxDriver
+		//System.setProperty("webdriver.gecko.driver", "C:\\Chromedriver\\geckodriver.exe");
+		//WebDriver driver1 = new FirefoxDriver();
+		
+		
+		
+		
+		driver.get("https://github.com/login");
+		driver.findElement(By.id("login_field")).sendKeys("mompalazov");
+		driver.findElement(By.id("password")).sendKeys("hello123");
+		driver.findElement(By.name("commit")).click();
+		//System.out.println(driver.getTitle());
+		//System.out.println(driver.getCurrentUrl());
+		//System.out.println(driver.getPageSource());
 		//driver.close();
 		//driver.quit();
 	

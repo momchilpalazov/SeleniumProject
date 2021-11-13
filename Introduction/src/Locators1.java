bb55185a044f600a628e8edf5e5ecb1d8296dd42
Palazov <Palazov@DESKTOP-J833GHP.fritz.box>
2021-11-13 14:44:21 +0100

    Sibling Parents to child and chiuld to parents

---
+++ Introduction/src/Locators1.java
@@ -0,0 +1,32 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
+				
+		WebDriver driver = new ChromeDriver();	
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		//Sibling-parents to child  traverse
+		////header/div/button[1]/following-sibling::button[1]
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		
+		//Sibling child to parent
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+		
+		
+	
+
+	}
+
+}

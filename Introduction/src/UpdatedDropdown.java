afe3281a05535d5820193e0a24398a57aadaa82b
Palazov <Palazov@DESKTOP-J833GHP.fritz.box>
2021-11-13 16:06:57 +0100

    Test Dropdwon looping UI

---
+++ Introduction/src/UpdatedDropdown.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");		
+		WebDriver driver = new ChromeDriver();			
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000L);
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		int i=1;
+		while(i<5)
+		{
+			driver.findElement(By.id("hrefIncAdt")).click();
+			i++;
+			
+		}
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+	}
+
+}

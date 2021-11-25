f629a4e0a032be04c34f3afaa2214b717fd562a0
Palazov <Palazov@DESKTOP-J833GHP>
2021-11-25 16:12:42 +0100

    Assert Checkbox

--- Introduction/src/Chekcbox.java
+++ Introduction/src/Chekcbox.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 
 public class Chekcbox {
 
@@ -11,25 +13,30 @@ public class Chekcbox {
 		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");		
 		WebDriver driver = new ChromeDriver();			
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		//Assert
+				
+		Assert.assertFalse(driver.findElement(By.cssSelector("label[for='ctl00_mainContent_chk_SeniorCitizenDiscount']")).isSelected());
+	
 		
 		System.out.println(	driver.findElement(By.cssSelector("label[for='ctl00_mainContent_chk_SeniorCitizenDiscount']")).isSelected());
 		driver.findElement(By.cssSelector("label[for='ctl00_mainContent_chk_SeniorCitizenDiscount']")).click();
 	    System.out.println(	driver.findElement(By.cssSelector("label[for='ctl00_mainContent_chk_SeniorCitizenDiscount']")).isSelected());
 	    
-	    //Count the number of checkbox
+	    //Count the number of check
 	   System.out.println( driver.findElement(By.cssSelector("input[type='checkbox']")).getSize());
 		
 		driver.findElement(By.id("divpaxinfo")).click();
 		Thread.sleep(2000L);
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 		int i=1;
-		while(i<5)
+		while(i<4)
 		{
 			driver.findElement(By.id("hrefIncAdt")).click();
 			i++;
 			
 		}
 		driver.findElement(By.id("btnclosepaxoption")).click();
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 
 	}

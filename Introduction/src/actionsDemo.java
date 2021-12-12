df9430d72329ec0cd606389fa6a48f3939bc9335
Palazov <Palazov@DESKTOP-J833GHP>
2021-12-12 20:20:41 +0100

    Handling Ajax/ Mouse interactions

---
+++ Introduction/src/actionsDemo.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class actionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.com/");
+		Actions a= new Actions(driver);
+		a.moveToElement(driver.findElement(By.id("nav-link-accountList-nav-line-1"))).build().perform();
+
+	}
+
+}

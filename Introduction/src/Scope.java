0331315b60ae90bba32a3de149ebfb473e88db0f
Palazov <Palazov@DESKTOP-J833GHP>
2021-12-19 22:47:21 +0100

    Click on each link in the coloumn and check if the pages are openig

--- Introduction/src/Scope.java
+++ Introduction/src/Scope.java
@@ -1,4 +1,7 @@
+import java.util.Iterator;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -26,7 +29,15 @@ public class Scope {
 		WebElement coloumndriver= footerwebelement.findElement(By.xpath("//body[1]/div[6]/table[1]/tbody[1]/tr[1]/td[1]"));
 		System.out.println(coloumndriver.findElements(By.tagName("a")).size());
 		
+		//4.Click on each link in the coloumn and check if the pages are openig
 		
+		for (int i = 0; i < coloumndriver.findElements(By.tagName("a")).size(); i++) {
+			
+			String clickOnLinkTab= Keys.chord(Keys.CONTROL,Keys.ENTER);
+			
+			coloumndriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLinkTab);
+			
+		} 
 
 	}
 

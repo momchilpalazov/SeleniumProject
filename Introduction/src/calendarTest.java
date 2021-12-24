b344d5320c2e5b7494c3016738cd973eed97717d
Palazov <Palazov@DESKTOP-J833GHP.fritz.box>
2021-12-24 17:26:36 +0100

    Current Date find

---
+++ Introduction/src/calendarTest.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class calendarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.path2usa.com/travel-companions");
+		
+		driver.findElement(By.id("ez-accept-all")).click();
+		driver.findElement(By.xpath("//input[@id='travel_date']")).click();
+		
+		List<WebElement> day=driver.findElements(By.className("day"));
+		int count=driver.findElements(By.className("day")).size();
+		
+		for (int i = 0; i < count ; i++) {
+			
+			String text=driver.findElements(By.className("day")).get(i).getText();
+			
+			if (text.equalsIgnoreCase("24")) {
+				driver.findElements(By.className("day")).get(i).click();
+				
+			}
+			
+		}
+		
+	}
+
+}

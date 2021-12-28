718b64336800f7d868da351faace2f075cfcf411
Palazov <Palazov@DESKTOP-J833GHP.fritz.box>
2021-12-28 14:16:51 +0100

    Table Grids Handle in webpage

--- Introduction/src/JavaScriptExecuter.java
+++ Introduction/src/JavaScriptExecuter.java
@@ -1,5 +1,9 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecuter {
@@ -18,7 +22,16 @@ public class JavaScriptExecuter {
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
 		
+		List<WebElement>values= driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
 		
+	  int sum=0;
+		for (int i = 0; i < values.size(); i++) {
+			
+			sum=sum+ Integer.parseInt( values.get(i).getText());
+			
+		}
+			
+		System.out.println(sum);
 
 	}
 

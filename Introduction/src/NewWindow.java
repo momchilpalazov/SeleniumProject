296bb006af4859a558b75e82eda57b79e2e57864
Palazov <Palazov@DESKTOP-J833GHP>
2022-01-04 16:19:45 +0100

    Taking WebElement Partial ScreenShot with selenium

--- Introduction/src/NewWindow.java
+++ Introduction/src/NewWindow.java
@@ -1,20 +1,26 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();		
 		driver.get("https://rahulshettyacademy.com/angularpractice/");
 		
+		//Switching WindowTab
 		driver.switchTo().newWindow(WindowType.TAB);
 		
 		Set<String>handles=	driver.getWindowHandles();
@@ -25,8 +31,15 @@ public class NewWindow {
 		driver.get("https://rahulshettyacademy.com/");
 		String courseName= driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
 		driver.switchTo().window(parentWindowsId);
-		driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
-		driver.quit();
+		WebElement name=driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		
+		//screenschot
+		File file=name.getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(file,new File("logo.png"));
+		
+		//driver.quit();
 
 	}
 

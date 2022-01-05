bc5d622ca41e21c21e5071df8a98f675b5165e09
Palazov <Palazov@DESKTOP-J833GHP>
2022-01-05 13:40:49 +0100

    TestNGTuturial

---
+++ TESTNG/src/test/Classtest1.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Classtest1 {
+	
+	
+	@Test
+	public void Demo()
+	{
+		System.out.println("Hello World");
+		
+	}
+
+@Test
+
+public void SecondTest()
+{
+	
+	System.out.println(" Ineed Learn");
+}
+
+}

506fd2f49910af9438af922e0df5c30256fc9648
Palazov <Palazov@DESKTOP-J833GHP>
2022-01-05 13:49:27 +0100

    Add Classess

---
+++ TESTNG/src/test/Classtest2.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Classtest2 {
+	@Test
+	public void WebloginCarLoan()
+	{
+		//selenium
+		System.out.println("WebLogin4");
+		
+	}
+	
+	@Test
+	public void MobileLiginCarLoan()
+	{
+		//Appium
+		System.out.println("MobileLogin4");
+		//hi
+	}
+	
+	@Test
+	public void LoginAPIcarLoan()
+	{
+		//RestAPI automation
+		System.out.println("RestApi4");
+	}
+
+}
---
+++ TESTNG/src/test/Classtest3.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Classtest3 {
+	
+	@Test
+	public void WebloginCarLoan()
+	{
+		//selenium
+		System.out.println("WebLogin4");
+		
+	}
+	
+	@Test
+	public void MobileLiginCarLoan()
+	{
+		//Appium
+		System.out.println("MobileLogin4");
+		//hi
+	}
+	
+	@Test
+	public void LoginAPIcarLoan()
+	{
+		//RestAPI automation
+		System.out.println("RestApi4");
+	}
+
+
+}
